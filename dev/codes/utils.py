1ff11368a16050d670f690fdb7adcb793be7a569
Preetham Ganesh <preetham.ganesh2021@gmail.com>
2022-09-10 19:40:41 -0500

    added check_directory_path_existence function.

---
+++ dev/codes/utils.py
@@ -0,0 +1,43 @@
+# authors_name = 'Preetham Ganesh'
+# project_title = 'Comparison of Approaches towards Classification of Birds Species'
+# email = 'preetham.ganesh2021@gmail.com'
+
+
+import os
+import logging
+import warnings
+
+
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+logging.getLogger("tensorflow").setLevel(logging.FATAL)
+warnings.filterwarnings("ignore")
+
+
+import json
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import figure
+import pandas as pd
+import time
+
+from model import BirdSpeciesClassification
+
+
+physical_devices = tf.config.list_physical_devices("GPU")
+tf.config.experimental.set_memory_growth(physical_devices[0], enable=True)
+
+
+def check_directory_path_existence(directory_path: str) -> str:
+    """Creates the absolute path for the directory path given in argument if it does not already exist.
+    Args:
+        directory_path: A string which contains the directory path that needs to be created if it does not already
+            exist.
+    Returns:
+        A string which contains the absolute directory path.
+    """
+    # Creates the following directory path if it does not exist.
+    home_directory_path = os.path.dirname(os.getcwd())
+    absolute_directory_path = "{}/{}".format(home_directory_path, directory_path)
+    if not os.path.isdir(absolute_directory_path):
+        os.makedirs(absolute_directory_path)
+    return absolute_directory_path
