7342cf0506b8d58427644d2e45e5ea498c139520
Preetham Ganesh <preetham.ganesh2021@gmail.com>
2022-09-10 22:28:38 -0500

    updated to pytest package

--- dev/codes/tests.py
+++ dev/codes/tests.py
@@ -5,34 +5,26 @@
 
 import os
 
-import unittest
+import pytest
 
 from utils import check_directory_path_existence
 
 
-class TestDevFunctions(unittest.TestCase):
-    """Tests functions in dev directory."""
+def test_check_directory_path_existence(self) -> None:
+    """Tests check_directory_path_existence function from utils.py with multiple test cases.
 
-    def test_check_directory_path_existence(self) -> None:
-        """Tests check_directory_path_existence function from utils.py with multiple test cases.
+    Args:
+        None.
 
-        Args:
-            None.
+    Returns:
+        None.
+    """
+    home_directory_path = os.path.dirname(os.getcwd())
 
-        Returns:
-            None.
-        """
-        home_directory_path = os.path.dirname(os.getcwd())
+    # Test case 1
+    directory_path = check_directory_path_existence(home_directory_path, "logs")
+    assert directory_path, "{}/logs".format(home_directory_path)
 
-        # Test case 1
-        directory_path = check_directory_path_existence(home_directory_path, "logs")
-        self.assertEqual(directory_path, "{}/logs".format(home_directory_path))
-
-        # Test case 2
-        directory_path = check_directory_path_existence(home_directory_path, "results")
-        self.assertEqual(directory_path, "{}/results".format(home_directory_path))
-
-
-if __name__ == '__main__':
-    unittest.main()
- 
+    # Test case 2
+    directory_path = check_directory_path_existence(home_directory_path, "results")
+    assert directory_path, "{}/results".format(home_directory_path)
